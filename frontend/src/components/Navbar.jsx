ac289c12dfe22cdb794f54e6dccc93b50ef9958b
Cherrypick14 <cherrypicklet@gmail.com>
2024-12-13 22:31:44 +0300

    feat: Add new fe files

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-center text-purple-800 font-bold' >Navbar</div>
+  )
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <>
+     <Navbar />
+     <div className='text-bold'>Write something in the home page</div>
+     <Footer />
+    </>
+   
+  )
+}
+
+export default Home;
