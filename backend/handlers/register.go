eb695a63f3e982d90e01df45b777372659739296
hilaokello <hilaryokello091@gmail.com>
2024-12-13 23:46:34 +0300

    feat: add a pay route

--- backend/handlers/register.go
+++ backend/handlers/register.go
@@ -10,8 +10,10 @@ func RegisterHandlers(mux *http.ServeMux, db *gorm.DB) {
 	profileHandler := &ProfileHandler{DB: db}
 	serviceHandler := &ServiceHandler{DB: db}
 	bookingHandler := &BookingHandler{DB: db}
+	paymentHandler := &PaymentHandler{DB: db}
 
 	mux.HandleFunc("/profile", profileHandler.CreateProfile)
 	mux.HandleFunc("/add-service", serviceHandler.AddServiceHandler)
 	mux.HandleFunc("/create-booking", bookingHandler.CreateBookingHandler)
+	mux.HandleFunc("/create-booking/pay", paymentHandler.CreatePaymentHandler)
 }
