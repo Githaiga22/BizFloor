a1f69d647c5fe672914e55eae714f3bffbd1c69f
hilaokello <hilaryokello091@gmail.com>
2024-12-14 06:06:09 +0300

    feat: business owner add service

--- backend/handlers/register.go
+++ backend/handlers/register.go
@@ -12,7 +12,7 @@ func RegisterHandlers(mux *http.ServeMux, db *gorm.DB) {
 	authHandler := auth.NewAuthHandler(db)
 	profileHandler := &ProfileHandler{DB: db}
 	businessProfileHandler := &BusinessProfileHandler{DB: db}
-	// serviceHandler := &ServiceHandler{DB: db}
+	serviceHandler := &ServiceHandler{DB: db}
 	// bookingHandler := &BookingHandler{DB: db}
 	// paymentHandler := &PaymentHandler{DB: db}
 
@@ -24,7 +24,7 @@ func RegisterHandlers(mux *http.ServeMux, db *gorm.DB) {
 	// Protected routes
 	mux.Handle("/api/profile", authHandler.AuthMiddleware(http.HandlerFunc(profileHandler.profileHandler)))
 	mux.Handle("/api/create-profile", authHandler.AuthMiddleware(http.HandlerFunc(businessProfileHandler.CreateProfile)))
-	// mux.Handle("/add-service", authHandler.AuthMiddleware(http.HandlerFunc(serviceHandler.AddServiceHandler)))
+	mux.Handle("/api/add-service", authHandler.AuthMiddleware(http.HandlerFunc(serviceHandler.AddServiceHandler)))
 	// mux.Handle("/create-booking", authHandler.AuthMiddleware(http.HandlerFunc(bookingHandler.CreateBookingHandler)))
 	// mux.Handle("/create-booking/pay", authHandler.AuthMiddleware(http.HandlerFunc(paymentHandler.CreatePaymentHandler)))
 
@@ -34,6 +34,7 @@ func RegisterHandlers(mux *http.ServeMux, db *gorm.DB) {
 	mux.Handle("/business-dashboard", authHandler.AuthMiddleware(http.HandlerFunc(serveTemplate("business-dashboard.html"))))
 	mux.Handle("/customer-dashboard", authHandler.AuthMiddleware(http.HandlerFunc(serveTemplate("customer-dashboard.html"))))
 	mux.Handle("/create-profile", authHandler.AuthMiddleware(http.HandlerFunc(serveTemplate("create-business-profile.html"))))
+	mux.Handle("/add-service", authHandler.AuthMiddleware(http.HandlerFunc(serveTemplate("add-service.html"))))
 }
 
 func serveTemplate(templateName string) http.HandlerFunc {
